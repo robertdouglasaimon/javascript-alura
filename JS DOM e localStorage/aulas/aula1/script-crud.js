29b5a1db53003407a3f869710bbe7caebc20776b
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-05-28 09:32:57 -0300

    UPDATE

---
+++ JS DOM e localStorage/aulas/aula1/script-crud.js
@@ -0,0 +1,21 @@
+// 1 - Encontrar o botão adicionar tarefa.  // 2 - Observar as suas classes e funcionalidades.
+
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const areaDeTextoDoFormulario = document.querySelector('.app__form-textarea');
+
+const listaDeTarefas = [];
+
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden');
+});
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: areaDeTextoDoFormulario.value 
+    }
+    listaDeTarefas.push(tarefa);
+    localStorage.setItem('listaDeTarefas', listaDeTarefas);
+});
+
