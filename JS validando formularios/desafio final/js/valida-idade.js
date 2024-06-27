f6cce77916f48f9569554873b15465b0f702cb30
robertdouglasaimon <robertdouglasaimon@gmail.com>
2024-06-26 21:27:39 -0300

    UPDATE

---
+++ JS validando formularios/desafio final/js/valida-idade.js
@@ -0,0 +1,16 @@
+export default function ehMaiorDeIdade(campo) {
+    const dataNascimento = new Date(campo.value);
+    if (!validaIdade(dataNascimento)) {
+        campo.setCustomValidity('O usuário não é maior de idade.')
+    }
+    // validaIdade(dataNascimento);
+
+    // console.log(validaIdade(dataNascimento));
+}
+
+function validaIdade(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataAtual >= dataMais18;
+}
